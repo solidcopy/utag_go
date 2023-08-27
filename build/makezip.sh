b84a66fde37ae4a9ee7322a36eb391f41af2e055
solidcopy <contact@solidcopy.net>
2023-08-27 16:33:10 +0900

    リリース用アーカイブの作成バッチを作成。

---
+++ build/makezip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir tmp
+GOOS=windows GOARCH=386 go build -ldflags='-w -s' -trimpath -o="tmp/utag-windows-386-$1.exe" cmd/utag/utag.go
+GOOS=windows GOARCH=amd64 go build -ldflags='-w -s' -trimpath -o="tmp/utag-windows-amd64-$1.exe" cmd/utag/utag.go
+GOOS=darwin GOARCH=amd64 go build -ldflags='-w -s' -trimpath -o="tmp/utag-darwin-amd64-$1" cmd/utag/utag.go
+GOOS=darwin GOARCH=arm64 go build -ldflags='-w -s' -trimpath -o="tmp/utag-darwin-arm64-$1" cmd/utag/utag.go
+GOOS=linux GOARCH=386 go build -ldflags='-w -s' -trimpath -o="tmp/utag-linux-386-$1" cmd/utag/utag.go
+GOOS=linux GOARCH=amd64 go build -ldflags='-w -s' -trimpath -o="tmp/utag-linux-amd64-$1" cmd/utag/utag.go
+zip -j utag-$1.zip tmp/utag-*
+rm -rf tmp
+
